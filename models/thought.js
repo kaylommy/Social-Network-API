cac7418d783890c2fb0615ecacc24b1174ce98ba
kaylommy <kaylajfreeman13@gmail.com>
2024-06-07 00:04:33 -0400

    filled out most of the models

---
+++ models/thought.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [
+            {
+                
+            }
+        ]
+    }
+)
